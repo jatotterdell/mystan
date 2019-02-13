470397940a0b9040ac4bbdccaeeeaeac313f0b20
James Totterdell <jatotterdell@gmail.com>
2019-02-13 10:00:43 +0800

    Init working package

---
+++ R/logreg_stan.R
@@ -0,0 +1,32 @@
+#' Bayesian logistic regression with Stan
+#'
+#' @export
+#' @param X Numeric matrix of design.
+#' @param y Numeric vector of responses.
+#' @param n Numeric vector of number of trials.
+#' @param ... Arguments passed to `rstan::sampling` (e.g. iter, chains).
+#' @return An object of class `stanfit` returned by `rstan::sampling`
+#'
+logreg_stan <- function(X, y, n, ...) {
+  standata <- list(X = X, y = y, n = n, N = length(y), K = ncol(X))
+  out <- rstan::sampling(stanmodels$log_reg, data = standata, ...)
+  return(out)
+}
+
+
+#' Bayesian logistic regression with Stan
+#'
+#' @export
+#' @param X Numeric matrix of design.
+#' @param y Numeric vector of responses.
+#' @param n Numeric vector of number of trials.
+#' @param Xpred Numeric matrix of prediction.
+#' @param npred Numeric vector of number of trials to predict.
+#' @param ... Arguments passed to `rstan::sampling` (e.g. iter, chains).
+#' @return An object of class `stanfit` returned by `rstan::sampling`
+#'
+logreg_stan_pred <- function(X, y, n, Xpred, npred, ...) {
+  standata <- list(X = X, y = y, n = n, N = length(y), K = ncol(X), Npred = nrow(Xpred), Xpred = Xpred, npred = npred)
+  out <- rstan::sampling(stanmodels$log_reg_pred, data = standata, ...)
+  return(out)
+}
