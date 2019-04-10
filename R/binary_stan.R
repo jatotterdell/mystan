7e8735136376a497d8697113be9445b638206370
James Totterdell <jatotterdell@gmail.com>
2019-04-10 19:04:20 +0800

    Update R functions.

--- R/binary_stan.R
+++ R/binary_stan.R
@@ -73,22 +73,3 @@ glm_betabinomial <- function(
   out <- fun(stanmodels$glm_betabinomial, data = standata, ...)
   return(out)
 }
-
-
-#' Bayesian logistic regression with Stan
-#'
-#' @export
-#' @param X Numeric matrix of design.
-#' @param y Numeric vector of responses.
-#' @param n Numeric vector of number of trials.
-#' @param Xpred Numeric matrix of prediction.
-#' @param npred Numeric vector of number of trials to predict.
-#' @param ... Arguments passed to `rstan::sampling` (e.g. iter, chains).
-#' @return An object of class `stanfit` returned by `rstan::sampling`
-#'
-logreg_stan_pred <- function(X, y, n, Xpred, npred, method = "sampling", ...) {
-  fun <- match.fun(method)
-  standata <- list(X = X, y = y, n = n, N = length(y), K = ncol(X), Npred = nrow(Xpred), Xpred = Xpred, npred = npred)
-  out <- fun(stanmodels$log_reg_pred, data = standata, ...)
-  return(out)
-}
--- R/survreg_stan.R
+++ R/survreg_stan.R
@@ -5,11 +5,21 @@
 #' @param y Response times
 #' @param v Censoring vector (0 = right-censored, 1 = exact time, 2 = left-censored, 3 = interval censored)
 #' @param rcens If interval censored data, then the right-censoring time
-#' @param ... Other arguments
-#' @return An object of class `stanfit` returned by `rstan::sampling`
-survreg_ph_exponential <- function(X, y, v, rcens = rep(NA, length(y)), sigma0 = 1, ...) {
-  standata <- list(X = X, y = y, v = v, rcens = rcens, N = length(y), K = ncol(X), sigma0 = sigma0)
-  out <- rstan::sampling(stanmodels$survreg_ph_exponential, data = standata, ...)
+#' @param mu0 Prior mean for regression coefficients.
+#' @param Sigma0 Prior covariance for regression coefficients.
+#' @param estimate Estimate the model (1) or just simulate data (0).
+#' @param ... Arguments passed to `rstan::sampling` (e.g. iter, chains).
+#' @return An object of class `stanfit` returned by `rstan`
+survreg_ph_exponential <- function(X, y, v,
+                                   rcens = rep(0, length(y)),
+                                   mu0 = rep(0, ncol(X)), Sigma0 = diag(1, ncol(X)),
+                                   estimate = 1,
+                                   method = "sampling", ...) {
+  fun <- match.fun(method)
+  standata <- list(X = X, y = y, v = v,
+                   rcens = rcens, N = length(y), K = ncol(X),
+                   mu0 = mu0, Sigma0 = Sigma0, estimate = estimate)
+  out <- fun(stanmodels$survreg_ph_exponential, data = standata, ...)
   return(out)
 }
 
